6ad0a0834390cc5e9ccbdb8e6f3f4d15845c3bb1
Stephen P <stephen@Stephens-MacBook-Pro.local>
2020-03-05 16:35:23 -0500

    Update

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session =boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -26,21 +27,32 @@ def list_bucket_objects(bucket):
 @click.argument('bucket')
 def setup_bucket(bucket):
         "Create and configure S3 bucket"
-        s3_bucket = s3.create_bucket(Bucket=bucket)
+        s3_bucket = None
+        try:
+                s3_bucket = s3.create_bucket(
+                        Bucket=bucket,
+                        CreateBucketConfiguration={'LocationConstraint': session.region_name}
+                )
+        except ClientError as e:
+                print(e.response)
+                if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+                        s3_bucket = s3.Bucket(bucket)
+                else:
+                        raise e
 
         policy = """
         {
-                "Version":"2012-10-17",
-                "Statement":[{
-	        "Sid":"PublicReadGetObject",
-                "Effect":"Allow",
-	        "Principal": "*",
-                        "Action":["s3:GetObject"],
-                        "Resource":["arn:aws:s3:::%s/*"
-                                ]
-                        }
-                ]
-        }
+         "Version":"2012-10-17",
+         "Statement":[{
+	 "Sid":"PublicReadGetObject",
+         "Effect":"Allow",
+	 "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"
+          ]
+         }
+        ]
+       }
         """ % s3_bucket.name
         policy = policy.strip()
 
